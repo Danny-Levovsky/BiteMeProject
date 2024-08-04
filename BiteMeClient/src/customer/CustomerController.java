4c356f1e0f2f20cbb6e7efe43a957ea3ecfb3d57
YosraDaso <80359742+YosraDaso@users.noreply.github.com>
2024-08-04 21:39:21 +0300

    newest version
    1- customer controller: logout + view order
    2- view order - basic

--- BiteMeClient/src/customer/CustomerController.java
+++ BiteMeClient/src/customer/CustomerController.java
@@ -1,78 +1,117 @@
 package customer;
 
-
-
-
+import client.ClientController;
+import login.LoginScreenController;
+import entites.Message;
 import entites.User;
+import enums.Commands;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+
+
+
+/**
+ * The CustomerController class handles the customer UI actions and interactions.
+ * This includes initializing the customer window and handling button actions for 
+ * logging out, creating new orders, and viewing existing orders to receiving them
+ * @author yosra
+ */
 public class CustomerController {
 
-    @FXML
-    private Label txtCustomerName; 
+	@FXML
+	private Label txtCustomerName;
 
-    @FXML
-    private Button btnLogout;
+	@FXML
+	private Button btnLogout;
 
-    @FXML
-    private Button BtnNewOrder;
+	@FXML
+	private Button BtnNewOrder;
 
-    @FXML
-    private Button BtnViewOrder;
-    
-    
-    private static User customer;
+	@FXML
+	private Button BtnViewOrder;
 
-    
-    public static void setCustomer(User user) {
-        customer = user;
-    }
-    
-    
-  	public void start(Stage primaryStage) throws Exception {
-  		FXMLLoader loader = new FXMLLoader(getClass().getResource("/customer/Customer.fxml"));
-      	Parent root = loader.load();
-      	Scene scene = new Scene(root);
-      	primaryStage.setTitle("CustomerWindow");
-      	primaryStage.setScene(scene);
-      	primaryStage.show();
-  	}
-  	
-    @FXML
-    private void initialize() {
-	  	 //update name
-	    if(customer != null) {
-	    	txtCustomerName.setText(customer.getFirstName() + " " + customer.getLastName());
-	    }
-    }
-    
+	private static User customer;
 
-    @FXML
-    void getBtnLogout(ActionEvent event) {
+	/**
+     * Sets the customer for the controller.
+     * @param user the customer user
+     */
+	public static void setCustomer(User user) {
+		customer = user;
+	}
 
-    }
+	/**
+     * Starts and displays the customer window.
+     * @param primaryStage the primary stage for this application
+     * @throws Exception if there is an error during the loading of the FXML file
+     */
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/customer/Customer.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("CustomerWindow");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-    @FXML
-    void getBtnNewOrder(ActionEvent event) {
+	/**
+     * Initializes the controller class. This method is automatically called after the
+     * FXML file has been loaded.
+     * it update the name to this user's name
+     */
+	@FXML
+	private void initialize() {
+		// update name
+		if (customer != null) {
+			txtCustomerName.setText(customer.getFirstName() + " " + customer.getLastName());
+		}
+	}
 
-    }
+	/**
+     * Handles the action for the logout button.
+     * This method is triggered when the logout button is clicked.
+     * It sends a logout request to the server and opens the login screen.
+     * @param event the event triggered by the logout button click
+     * @throws Exception if there is an error while opening the login screen
+     */
+	@FXML
+	void getBtnLogout(ActionEvent event) throws Exception {
+		Message logoutMessage = new Message(CustomerController.customer.getId(), Commands.LogoutUser);
+		ClientController.client.sendToServer(logoutMessage);
 
-    @FXML
-    void getBtnViewOrder(ActionEvent event) {
+		((Node) event.getSource()).getScene().getWindow().hide();
+		LoginScreenController newScreen = new LoginScreenController();
+		newScreen.start(new Stage());
+	}
 
-    }
+	@FXML
+	void getBtnNewOrder(ActionEvent event) {
 
+	}
 
-    
-    
+	/**
+     * Handles the action for the view order button.
+     * This method is triggered when the view order button is clicked.
+     * It hides the current window and opens the view order screen.
+     * @param event the event triggered by the view order button click
+     * @throws Exception if there is an error while opening the view order screen
+     */
+	@FXML
+	void getBtnViewOrder(ActionEvent event) throws Exception {
+		ViewOrderController.setId(customer.getId());
+		((Node) event.getSource()).getScene().getWindow().hide();
+		ViewOrderController newScreen = new ViewOrderController();
+		newScreen.start(new Stage());
 
+	}
 
 }
--- BiteMeClient/src/customer/ViewOrderController.java
+++ BiteMeClient/src/customer/ViewOrderController.java
@@ -1,18 +1,52 @@
 package customer;
 
-
+import entites.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
 
 public class ViewOrderController {
 
+	@FXML
+	private Button btnBack;
     @FXML
-    private Button btnBack;
+    private Button btnApprove;
+    
+    private static int id;
+    
+	public static void setId(int id1) {
+		id = id1;
+	}
 
+
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/customer/ViewOrder.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("ViewOrderWindow");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	
     @FXML
-    void getBtnBack(ActionEvent event) {
+    void getBtnApprove(ActionEvent event) {
 
     }
+
+	@FXML
+	void getBtnBack(ActionEvent event) throws Exception {
+		((Node) event.getSource()).getScene().getWindow().hide();
+		CustomerController newScreen = new CustomerController();
+		newScreen.start(new Stage());
+	}
 
 }
